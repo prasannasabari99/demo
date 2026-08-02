0716d429c0cdc60099517a748b56ca10f3705bdf
prasannasabari99 <sabaripavi99@gmail.com>
2026-08-02 08:45:59 +0530

    reset commit

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+console.log("welcome to git course in playlist")
 
+for (let index = 0; index < 5; index++) {
+    console.log("hi");
+   
 
+}
 
 
 
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 #git courser
 
 sub1 branch added 
+
+added some notes in sub2
